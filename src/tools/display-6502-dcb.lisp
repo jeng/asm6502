6b6e54814f3a70041ae774aa20bab4c3554da4e7
J English <jhe@localhost.localdomain>
2008-01-13 08:44:52 -0600

    I bumped the version number of the debugger, deleted the duplicate assembly fileand added a copywrite statement to the dcb dumper.

--- src/tools/display-6502-dcb.lisp
+++ src/tools/display-6502-dcb.lisp
@@ -1,3 +1,21 @@
+;;;; Copyright (c) Jeremy English <jhe@jeremyenglish.org>
+
+;;;; Permission to use, copy, modify, distribute, and sell this
+;;;; software and its documentation for any purpose is hereby granted
+;;;; without fee, provided that the above copyright notice appear in
+;;;; all copies and that both that copyright notice and this
+;;;; permission notice appear in supporting documentation.  No
+;;;; representations are made about the suitability of this software
+;;;; for any purpose.  It is provided "as is" without express or
+;;;; implied warranty.
+
+;;;; Created: 13-January-2008
+
+;;;; This program will dump all of the dcb statements in a assemble
+;;;; file to a png file. It also has a function that will print the
+;;;; dcb statement aligned to the column limit of m6502.
+
+
 (defpackage dcb-data
   (:use #:cl #:cl-ppcre #:zpng))
 (in-package :dcb-data)
